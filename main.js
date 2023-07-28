876c792f1d71ed563c1082e9b59d595b53af52c0
izzatmammadov <imamedov388@gmail.com>
2023-07-28 19:02:56 +0400

    update

--- main.js
+++ main.js
@@ -6,17 +6,50 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
-// intro page codes 
+// intro page codes
 
-function startGame() {
-    intro.style.display="none";
-    game.style.display="block";
+function playGame() {
+  intro.style.display = "none";
+  game.style.display = "block";
 }
 
 function cancelGame() {
-    alert("YOU ARE A LOSER!")
+  alert("YOU ARE A LOSER!");
 }
 
 // game page codes
 
-let = ['rock', 'paper', 'scissors']
+let youScore = 0;
+let compScore = 0;
+
+let hands = ["r", "p", "s"];
+
+function randomHandSelector(arr) {
+  let randomHand = Math.floor(Math.random() * arr.length);
+  return arr[randomHand];
+}
+
+let compChoose = randomHandSelector(hands);
+
+function startGame(e) {
+  let yourChoose = e.key;
+
+  if (!hands.includes(yourChoose)) {
+    alert("PLEASE CHOOSE CORRET ITEM 'R' rock, 'P' paper, 'S' scissors");
+    return;
+  }
+}
+
+window.onkeydown = startGame;
+console.log(yourChoose);
+
+console.log(compChoose);
+
+// function showHands (you,computer) {
+//     humanScore.innerHTML = you;
+//     computerScore.innerHTML = computer;
+
+//     humanScore.src = `./images/${you}.png`;
+//     computerScore.src = `./images/${computer}.png`;
+
+// }
